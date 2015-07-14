a15d44aac6f70d7464d312b4c0e6678631edcc3e
Rickard Edén <rickardeden@gmail.com>
2015-07-14 11:27:22 +0200

    New CardboardHarnessFragment.

--- src/com/jme3/system/android/CardboardContext.java
+++ src/com/jme3/system/android/CardboardContext.java
@@ -19,6 +19,7 @@ import com.google.vrtoolkit.cardboard.HeadTransform;
 import com.google.vrtoolkit.cardboard.Viewport;
 import com.google.vrtoolkit.cardboard.sensors.HeadTracker;
 import com.jme3.input.android.AndroidInputHandler;
+import com.jme3.input.android.AndroidInputHandler14;
 import com.jme3.math.Matrix4f;
 import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
@@ -66,31 +67,27 @@ public class CardboardContext extends OGLESContext implements CardboardView.Rend
         params.setInterLensDistance(0.45f);
         params.setScreenToLensDistance(0.045f);
 
+
         mHMDManager = new HeadMountedDisplayManager(context);
         mHMDManager.getHeadMountedDisplay().setCardboardDeviceParams(params);
 //        mHMDManager.getHeadMountedDisplay();
         // Start to set up the view
         mView = new CardboardView(context);
         if (androidInput == null) {
-            androidInput = new AndroidInputHandler();
+            if (Build.VERSION.SDK_INT >= 14) {
+                androidInput = new AndroidInputHandler14();
+            } else if (Build.VERSION.SDK_INT >= 9){
+                androidInput = new AndroidInputHandler();
+            }
         }
         androidInput.setView(mView);
         androidInput.loadSettings(settings);
 
-        // setEGLContextClientVersion must be set before calling setRenderer
-        // this means it cannot be set in AndroidConfigChooser (too late)
         mView.setEGLContextClientVersion(2);
 
         mView.setFocusableInTouchMode(true);
         mView.setFocusable(true);
 
-        // setFormat must be set before AndroidConfigChooser is called by the surfaceview.
-        // if setFormat is called after ConfigChooser is called, then execution
-        // stops at the setFormat call without a crash.
-        // We look at the user setting for alpha bits and set the surfaceview
-        // PixelFormat to either Opaque, Transparent, or Translucent.
-        // ConfigChooser will do it's best to honor the alpha requested by the user
-        // For best rendering performance, use Opaque (alpha bits = 0).
         int curAlphaBits = settings.getAlphaBits();
         logger.log(Level.FINE, "curAlphaBits: {0}", curAlphaBits);
         if (curAlphaBits >= 8) {
@@ -108,11 +105,7 @@ public class CardboardContext extends OGLESContext implements CardboardView.Rend
         AndroidConfigChooser configChooser = new AndroidConfigChooser(settings);
         mView.setEGLConfigChooser(configChooser);
         mView.setRenderer((Renderer)this);
-
-        // Attempt to preserve the EGL Context on app pause/resume.
-        // Not destroying and recreating the EGL context
-        // will help with resume time by reusing the existing context to avoid
-        // reloading all the OpenGL objects.
+        
         if (Build.VERSION.SDK_INT >= 11) {
             mView.setPreserveEGLContextOnPause(true);
         }
